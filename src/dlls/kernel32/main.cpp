c295b71b497ec76a0d72c9b34a785c578bdd225b
welrox <osamabadeeb2@gmail.com>
2025-01-20 20:24:15 +0000

    refactor kernel32 to use patch_dll_imports

--- src/dlls/kernel32/main.cpp
+++ src/dlls/kernel32/main.cpp
@@ -13,6 +13,7 @@
 #include <chrono>
 #include <unistd.h>
 #include <sys/mman.h>
+#include "../common.h"
 #include "../../pe.h"
 #include "../asm.h"
 #include "../cp437.h"
@@ -542,7 +543,7 @@ EXPORT __attribute__((naked)) void GetProcAddress()
 
 __attribute__((constructor)) void start()
 {
-    printf("kernel32 starting!\n");
+    printf("kernel32 entry\n");
     #define IMPORT_ENTRY(name) {#name, reinterpret_cast<uintptr_t>(name)}
     const std::map<std::string, uintptr_t> import_name_to_fn = {
         IMPORT_ENTRY(Sleep), IMPORT_ENTRY(ExitProcess), IMPORT_ENTRY(EnterCriticalSection), IMPORT_ENTRY(DeleteCriticalSection),
@@ -554,129 +555,11 @@ __attribute__((constructor)) void start()
     };
     #undef IMPORT_ENTRY
 
-    const struct section_64* header_cmd = getsectbyname("__TEXT", "___header");
-    if (!header_cmd)
-    {
-        printf("kernel32 error: could not find section ___header, exiting\n");
-        std::exit(1);
-    }
-    const struct section_64* entry_cmd = getsectbyname("__TEXT", "___entry");
-    if (!entry_cmd)
-    {
-        printf("kernel32 error: could not find section ___entry, exiting\n");
-        std::exit(1);
-    }
-
-    if (entry_cmd->size != 61)
-    {
-        throw std::runtime_error("custom entry code size has changed -- update kernel32 to work with the new code");
-    }
-
-    header_addr = header_cmd->addr;
-    uint32_t memcpy_offset = reinterpret_cast<uintptr_t>(__memcpy_kernel32_startup_impl) - (entry_cmd->addr + 0x1e) - 5;
+    section_64 header_cmd;
+    section_64 entry_cmd;
+    patch_dll_imports("kernel32.dll", import_name_to_fn, &header_cmd, &entry_cmd);
+    header_addr = header_cmd.addr;
+    uint32_t memcpy_offset = reinterpret_cast<uintptr_t>(__memcpy_kernel32_startup_impl) - (entry_cmd.addr + 0x1e) - 5;
     printf("kernel32: patching memcpy into entry\n");
-    memcpy(reinterpret_cast<void*>(entry_cmd->addr + 0x1f), &memcpy_offset, sizeof(memcpy_offset));
-    
-    constexpr int exe_image_index = 0;
-    uintptr_t exe_base = reinterpret_cast<uintptr_t>(_dyld_get_image_header(exe_image_index));
-    uintptr_t exe_slide = _dyld_get_image_vmaddr_slide(exe_image_index);
-    printf ("image %d: %p\t%s\t(slide = 0x%lx)\n", exe_image_index,
-    reinterpret_cast<void*>(exe_base),
-    _dyld_get_image_name(exe_image_index),
-    exe_slide);
-
-    IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*)header_addr;
-    __IMAGE_NT_HEADERS64* nt_header = (__IMAGE_NT_HEADERS64*)(header_addr + dos_header->e_lfanew);
-    uintptr_t import_addr = exe_base + nt_header->OptionalHeader.DataDirectory[___IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
-
-    printf("kernel32: parsing imports\n");
-    printf(" import_addr %lx\n", import_addr);
-
-    for (IMAGE_IMPORT_DESCRIPTOR* import_descriptor = reinterpret_cast<IMAGE_IMPORT_DESCRIPTOR*>(import_addr + exe_slide);
-    import_descriptor->OriginalFirstThunk != 0; import_descriptor++)
-    {
-        std::string dll_name = reinterpret_cast<char*>(exe_base + import_descriptor->Name);
-        if (strcasecmp(dll_name.c_str(), "kernel32.dll") != 0)
-            continue;
-
-        for (uintptr_t* thunk = reinterpret_cast<uintptr_t*>(exe_base + import_descriptor->FirstThunk);
-        *thunk != 0; thunk++)
-        {
-            uintptr_t thunk_val = *thunk;
-            if (thunk_val & (1ull << 63))
-            {
-                std::cerr << "Warning: Ordinal detected! ignoring...\n";
-            }
-            else
-            {
-                IMAGE_IMPORT_BY_NAME* hint_name = reinterpret_cast<IMAGE_IMPORT_BY_NAME*>(exe_base + thunk_val);
-                std::string import_fn_name = reinterpret_cast<char*>(hint_name->Name);
-                if (import_name_to_fn.find(import_fn_name) != import_name_to_fn.end())
-                {
-                    uintptr_t fn = import_name_to_fn.at(import_fn_name);
-                    *thunk = fn;
-                    printf("Fixed %s import (%lx)\n", import_fn_name.c_str(), *thunk);
-                }
-                else
-                {
-                    printf("kernel32: warning: unimplemented function %s\n", import_fn_name.c_str());
-
-                    // Replace the unimplemented import with a function that prints its name for convenience
-                    const BYTE unimplemented_fn_code[] = 
-                    "\x48\x8B\x34\x24"                         // 00: mov    rsi, return_address (2nd argument for printf)
-                    "\x48\xBF\x00\x00\x00\x00\x00\x00\x00\x00" // 04: movabs rdi, format_string (format string for printf)
-                    "\xB0\x00"                                 // 0e: mov al, 0x0 (number of float args for System V calling conv)
-                    "\x53"                                     // 10: push rbx
-                    "\x48\xBB\x00\x00\x00\x00\x00\x00\x00\x00" // 11: movabs rbx, printf
-                    "\xFF\xD3"                                 // 1b: call rbx
-                    "\x5B"                                     // 1d: pop rbx
-                    "\xC3"                                     // 1e: ret
-                    ;
-
-                    uintptr_t printf_address = (uintptr_t)printf;
-
-                    // Construct the format string
-                    const char msg1[] = "*** unimplemented kernel32 function: ";
-                    char* unimplemented_fn_msg = (char*)mmap(nullptr, 256, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-                    if ((intptr_t)unimplemented_fn_msg == -1)
-                    {
-                        perror("mmap failed");
-                        std::exit(1);
-                    }
-                    memset(unimplemented_fn_msg, 0, 256);
-                    strcpy(unimplemented_fn_msg, msg1);
-                    strcpy(unimplemented_fn_msg + strlen(msg1), (import_fn_name + " : return address %p\n").c_str());
-
-                    // Allocate the buffer for the unimplemented function
-                    char* unimplemented_fn_buffer = (char*)mmap(nullptr, 256, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-                    if ((intptr_t)unimplemented_fn_buffer == -1)
-                    {
-                        perror("mmap failed");
-                        std::exit(1);
-                    }
-
-                    if (mprotect((void*)((uintptr_t)unimplemented_fn_buffer & ~(0x1000ul - 1ul)), 0x1000, PROT_READ | PROT_WRITE) == -1)
-                    {
-                        perror("mprotect failed");
-                        std::exit(1);
-                    }
-
-                    // Do the necessary patching
-                    memcpy(unimplemented_fn_buffer, unimplemented_fn_code, sizeof(unimplemented_fn_code) - 1);
-                    memcpy(unimplemented_fn_buffer + 6, &unimplemented_fn_msg, sizeof(const char*));
-                    memcpy(unimplemented_fn_buffer + 0x13, &printf_address, sizeof(printf_address));
-
-                    if (mprotect((void*)((uintptr_t)unimplemented_fn_buffer & ~(0x1000ul - 1ul)), 0x1000, PROT_READ | PROT_EXEC) == -1)
-                    {
-                        perror("mprotect failed");
-                        std::exit(1);
-                    }
-
-                    *thunk = reinterpret_cast<uintptr_t>(unimplemented_fn_buffer);
-                }
-            }
-        }
-    }
-
-    printf("kernel32: DONE!\n");
+    memcpy(reinterpret_cast<void*>(entry_cmd.addr + 0x1f), &memcpy_offset, sizeof(memcpy_offset));
 }
